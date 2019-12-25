d4a38057cd24c5ba793c7b27d77f68a5fc3436ec
Dmytro Boriskin <dmytro.boriskin@gmail.com>
2019-12-25 10:43:29 +0200

    HW6_eslint check

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -6,31 +6,33 @@ let x;
 let x1;
 let x2;
 
-a = prompt("Please insert the 1st value", 1);
-while( (isNaN(a)) || (Number.isInteger(a)) ) {
-    a = prompt("Invalid input data. Please enter a valid and integer number", 1);
+a = prompt('Please insert the 1st value', 0);
+while( isNaN(a) || Number.isInteger(a) ) {
+    a = prompt('Invalid input data. Please enter a valid and integer number', 0);
 }
 
-b = prompt("Please insert the 2nd value", 2);
-while( (isNaN(b)) || (Number.isInteger(b)) ) {
-    b = prompt("Invalid input data. Please enter a valid and integer number", 2);
+b = prompt('Please insert the 2nd value', 0);
+while( isNaN(b) || Number.isInteger(b) ) {
+    b = prompt('Invalid input data. Please enter a valid and integer number', 0);
 }
 
-c = prompt("Please insert the 3rd value", 1);
-while( (isNaN(c)) || (Number.isInteger(c)) ) {
-    c = prompt("Invalid input data. Please enter a valid and integer number", 3);
+c = prompt('Please insert the 3rd value', 0);
+while( isNaN(c) || Number.isInteger(c) ) {
+    c = prompt('Invalid input data. Please enter a valid and integer number', 0);
 }
 
-d = b * b - 4 * a * c;
+let q = 4;
+d = b * b - q * a * c;
 console.log(`Discriminant = ${d}`);
 
+let q1 = 2;
 if ( d > 0 ) {
-   x1 = Math.round((-b + Math.sqrt(d)) / 2 * a);
-   x2 = Math.round((-b - Math.sqrt(d)) / 2 * a);
+   x1 = Math.round((-b + Math.sqrt(d)) / q1 * a);
+   x2 = Math.round((-b - Math.sqrt(d)) / q1 * a);
    console.log(`x1 =  ${x1}, x2 = ${x2}`)
-} else if (d == 0) {
-    x = (-b / (2 * a));
+} else if (d === 0) {
+    x = -b / (q1 * a);
     console.log(`x = ${x}`);
 } else {
-    console.log("No solution");
+    console.log('No solution');
 }
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -2,32 +2,32 @@ let a;
 let b;
 let c;
 
-a = parseInt(prompt("Please insert the 1st value", 7));
-while ((isNaN(a))) {
-    a = parseInt(prompt("Input values should be ONLY numbers. Please enter a valid number", 7));
+a = parseInt(prompt('Please insert the 1st value', 0));
+while (isNaN(a)) {
+    a = parseInt(prompt('Input values should be ONLY numbers. Please enter a valid number', 0));
 }
 
-b = parseInt(prompt("Please insert the 2nd value", 8));
-while ((isNaN(b))) {
-    b = parseInt(prompt("Input values should be ONLY numbers. Please enter a valid number", 8));
+b = parseInt(prompt('Please insert the 2nd value', 0));
+while (isNaN(b)) {
+    b = parseInt(prompt('Input values should be ONLY numbers. Please enter a valid number', 0));
 } 
 
-c = parseInt(prompt("Please insert the 3rd value", 9));
-while ((isNaN(c))) {
-    c = parseInt(prompt("Input values should be ONLY numbers. Please enter a valid number", 9));
+c = parseInt(prompt('Please insert the 3rd value', 0));
+while (isNaN(c)) {
+    c = parseInt(prompt('Input values should be ONLY numbers. Please enter a valid number', 0));
 } 
 
 console.log(`Side a = ${a}, side b = ${b}, side c = ${c}`);
 
 if (a <= 0 || b <= 0 || c <= 0) {
-    alert("A triangle must have 3 sides with a positive definite length");
+    alert('A triangle must have 3 sides with a positive definite length');
 } else if (a + b <= c || a + c <= b || b + c <= a) {
-    alert("Triangle doesn’t exist");
-    console.log("Triangle doesn’t exist");
+    alert('Triangle doesn’t exist');
+    console.log('Triangle doesn’t exist');
 } else if (a === b && b === c) {
-    console.log("Equilateral triangle");
+    console.log('Equilateral triangle');
 } else if (a === b || a === c || b === c) {
-    console.log("Isosceles triangle");
+    console.log('Isosceles triangle');
 } else {
-    console.log("Scalene triangle");
+    console.log('Scalene triangle');
 } 
