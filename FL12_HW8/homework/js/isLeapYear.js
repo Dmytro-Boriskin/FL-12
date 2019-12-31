daa4bf0bba757dccb7732b79a6e1bd6edc0a66c1
Dmytro Boriskin <dmytro.boriskin@gmail.com>
2019-12-31 15:21:58 +0200

    HW8 complete

--- FL12_HW8/homework/js/isLeapYear.js
+++ FL12_HW8/homework/js/isLeapYear.js
@@ -1,1 +1,21 @@
-// to be added...
+const isLeapYear = (fullDate) => {
+    let date = new Date(fullDate);
+    let year = date.getFullYear();
+
+    if (year) {
+        if (!((year % 4) || (!(year % 100) && (year % 400)))) {
+            console.log(year + ' is a leap year');
+        } else {
+            console.log(year + ' is not a leap year');
+        }
+    } else {
+        console.log('Invalide date');
+    }
+}
+
+isLeapYear('2020-01-01 00:00:00');
+isLeapYear('2020-01-01 00:00:00777');
+isLeapYear('2021-01-15 13:00:00');
+isLeapYear('2200-01-15 13:00:00');
+isLeapYear(1213131313135465656654564646542132132131);
+isLeapYear(1213131313);
